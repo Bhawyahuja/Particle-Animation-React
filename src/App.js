64b8b6ed48a4fa25805f96363fce8d40c0d8883e
Bhawyahuja <60682955+Bhawyahuja@users.noreply.github.com>
2021-07-31 00:09:49 +0530

    beautified

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ function App() {
       <div id= "text_div" >
         <div className= "center_all" >
           <h1 className="title_text" >WELCOME!</h1>
-          
-
         </div>
         <p className="hover_text" >Hover mouse to enjoy particle effect</p>
       </div>
